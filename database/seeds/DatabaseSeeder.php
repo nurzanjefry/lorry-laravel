dca2f1ad9e3e36e0363b977f55abf8aaaa0ca2f5
nurzan <nurzan.dev@gmail.com>
2020-10-29 15:26:52 +0000

    added seeder for user1 as admin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'User1',
+            'role' => 1,
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+        ]);
     }
 }
