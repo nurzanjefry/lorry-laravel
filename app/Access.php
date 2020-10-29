fd3517ee91557b4153063b1cd3bb3fcaab46feeb
nurzan <nurzan.dev@gmail.com>
2020-10-29 15:25:46 +0000

    add fillable table

--- app/Access.php
+++ app/Access.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Access extends Model
 {
-    //
+    protected $table = 'access';
+
+    public $incrementing = true;
+
+    protected $dates = ['created_at','updated_at','deleted_at'];
+
+    protected $fillable = [ 
+                            'user_id', 
+                            'role_id', 
+                            'active', 
+                            'remarks', 
+                            'created_at', 
+                            'created_id', 
+                            'updated_at', 
+                            'updated_id', 
+                            'deleted_at', 
+                            'deleted_id'
+                            ];
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = 'posts';
+
+    public $incrementing = true;
+
+    protected $dates = ['created_at','updated_at','deleted_at'];
+
+    protected $fillable = [ 
+                            'title', 
+                            'content', 
+                            'posted', 
+                            'remarks', 
+                            'created_at', 
+                            'created_id', 
+                            'updated_at', 
+                            'updated_id', 
+                            'deleted_at', 
+                            'deleted_id'
+                            ];
 }
