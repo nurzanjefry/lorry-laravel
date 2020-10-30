2e2c4ff08a0e5095676d94901c717029cc2124dc
nurzan <nurzan.dev@gmail.com>
2020-10-30 09:05:41 +0000

    add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,16 @@ class DatabaseSeeder extends Seeder
             'role' => 1,
             'email' => 'admin@admin.com',
             'password' => Hash::make('admin'),
+            'created_at' => Carbon::now(),
+            'created_id' => 0,
+        ]);
+
+        DB::table('users')->insert([
+            'role' => 1,
+            'remarks' => 'admin',
+            'active' => true,
+            'created_at' => Carbon::now(),
+            'created_id' => 0,
         ]);
     }
 }
